d73a368b50ec7c6b5725bd4692f4f88143c861c7
islem7othmani <islemothmani159@gmail.com>
2022-07-06 16:09:14 +0100

    api gallery

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import axios from 'axios';
 function App() {
+  const [Photo, setPhoto] = useState("");
+  const [clientId, setclienId] = useState(
+    "O95FLnUmi9JM0NfbbmyKfee9qBtc1GbZ02fGOKL3jnE"
+  );
+  const [result, setResult] = useState([]);
+
+  function handleChange(event) {
+    setPhoto(event.target.value);
+  }
+  function handleSubmit(event) {
+    console.log(Photo);
+    const url =
+      "https://api.unsplash.com/search/photos?query=" +
+      Photo +
+      "&client_id=" +
+      clientId;
+    axios.get(url).then((response) => {
+      setResult(response.data.results);
+      console.log(response);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="title">Search Images</div>
+      <div className="content">
+      <input type="text" name="photo" onChange={handleChange} />
+      <button onClick={handleSubmit}>search</button>
+      </div>
+      <div className="main">
+      <div className="imagesoutput">
+      {result.map((Photo) => (
+        <img src={Photo.urls.small} alt="imag" />
+      ))}
+      </div>
+    </div>
     </div>
   );
 }
